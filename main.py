ba715bab26906159a5e14fbdd1bfd58123229f7b
david <davidlouie45@gmail.com>
2018-05-05 19:39:29 -0700

    Changed colors to be consistent across functions

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def freq_by_day(person):
     objects = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
     y_pos = np.arange(len(objects))
     freq = person.get_days().values()
-    plt.bar(y_pos, freq, align='center', alpha=0.5)
+    plt.bar(y_pos, freq, align='center', alpha=0.5, color=col_map[person.get_name()])
     plt.xticks(y_pos, objects)
     plt.ylabel('Frequency')
     plt.title(person.get_name() + ' message frequency by weekday')
@@ -56,7 +56,7 @@ def freq_chrono(person):
     date_list = convert_timestamps(person.dates)
     y_pos = np.arange(len(date_list))
     freq = count_timestamps(person.dates, len(date_list))
-    plt.bar(y_pos, freq, align='center', alpha=0.5)
+    plt.bar(y_pos, freq, align='center', alpha=0.5, color=col_map[person.get_name()])
 
     # produce 5 or 6 evenly spaced x-axis labels
     plt.xticks(create_ind(date_list), create_labels(date_list))
@@ -78,7 +78,7 @@ def freq_chrono_total(people):
 
 
 # create stacked bar chart of message frequency over the dataset
-def freq_chrono_stacked(people, color_map):
+def freq_chrono_stacked(people):
     combined_ts = []
     for p in people:
         combined_ts.extend(p.dates)
@@ -94,7 +94,7 @@ def freq_chrono_stacked(people, color_map):
     first_p = people[0]
     prev = count_timestamps(first_p.dates, len(date_list))
     plt.bar(x_ind, prev, width,
-            color=color_map[first_p.get_name()],
+            color=col_map[first_p.get_name()],
             label=first_p.get_name())
 
     # plot the rest of the people, skipping the first person
@@ -103,7 +103,7 @@ def freq_chrono_stacked(people, color_map):
     for p in iter_people:
         p_date_freq = count_timestamps(p.dates, len(date_list))
         plt.bar(x_ind, p_date_freq, width, bottom=prev,
-                color=color_map[p.get_name()],
+                color=col_map[p.get_name()],
                 label=p.get_name())
         prev = list(map(sum,  zip(prev, p_date_freq)))
 
@@ -129,16 +129,17 @@ def get_colors(num_colors):
 
 # assign each person a unique color to display in the graph
 def _assign_colors(people):
-    colors = get_colors(len(people))
+    colors = get_colors(len(people) + 1)
     objects = {}
     for i in range(0, len(people)):
         p = people[i]
         objects[p.get_name()] = colors[i]
+    objects['Total'] = colors[len(people)]      # color of combined data
     return objects
 
 
 # bar chart of average usage of a stat determined by func by person
-def freq_non_word(people, color_map, func, message_type):
+def freq_non_word(people, func, message_type):
     # create plot
     plt.subplots()
     bar_width = 0.1
@@ -152,7 +153,7 @@ def freq_non_word(people, color_map, func, message_type):
         p_data = (func(p))
         plt.bar(i*bar_width, p_data, bar_width,
                 alpha=opacity,
-                color=color_map[p.get_name()],
+                color=col_map[p.get_name()],
                 label=p.get_name())
 
     plt.xlabel(message_type)
@@ -186,15 +187,15 @@ def produce_table(people):
 if __name__ == "__main__":
     m_p = MessageParser('/home/david/Documents/Personal_Projects/facebookData/messages/dontmebro_08776cf081/message.json', 'David Louie')
     m_p.parse()
-    # freq_by_day(m_p.get_people()[0])
-    # freq_chrono(m_p.get_people()[2])
-    # freq_chrono_total(m_p.get_people())
     col_map = _assign_colors(m_p.get_people())
-    freq_chrono_stacked(m_p.get_people(), col_map)
-    # freq_non_word(m_p.get_people(), color_map, Person.get_stick_avg, 'Stickers')
-    # freq_non_word(m_p.get_people(), color_map, Person.get_photos_avg, 'Photos')
-    # freq_non_word(m_p.get_people(), color_map, Person.get_gifs_avg, 'Gifs')
-    # produce_table(m_p.get_people())
+    freq_by_day(m_p.get_people()[0])
+    freq_chrono(m_p.get_people()[2])
+    freq_chrono_total(m_p.get_people())
+    freq_chrono_stacked(m_p.get_people())
+    freq_non_word(m_p.get_people(), Person.get_stick_avg, 'Stickers')
+    freq_non_word(m_p.get_people(), Person.get_photos_avg, 'Photos')
+    freq_non_word(m_p.get_people(), Person.get_gifs_avg, 'Gifs')
+    produce_table(m_p.get_people())
     m_p.pretty_print()
 
 
